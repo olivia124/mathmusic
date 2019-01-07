1bc73e810ab274e328a38cf95431f2391844d35d
olivia124 <46164678+olivia124@users.noreply.github.com>
2019-01-07 16:13:14 +0100

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,10 @@
+x = input("El nombre del personaje que liga astronomia, música y queen es: ")
+
+while x != str("brian may"):
+    x = input("El nombre del personaje que liga astronomia, música y queen es: ")
+if x == str("brian may"):
+        print('https://www.youtube.com/watch?v=j3Jm5POCAj8')
+
+
+
+input("Presiona intro para cerrar la ventana")
